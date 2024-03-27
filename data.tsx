d33b4e524331403ad3c54f3d8c68931f9eecd7a2
Ramses Sotomayor <chiconucks@gmail.com>
2024-03-27 11:32:23 -0400

    project

---
+++ data.tsx
@@ -0,0 +1,211 @@
+import { BookText, CodeSquare, HomeIcon, UserRound, Linkedin, Twitter, Rss, Crop, Pencil, Computer, Book, Rocket, Speech } from "lucide-react";
+
+export const socialNetworks = [
+
+    {
+        id: 1,
+        logo: <Linkedin size={30} strokeWidth={1} />,
+        src: "#!",
+    },
+    {
+        id: 2,
+        logo: <Twitter size={30} strokeWidth={1} />,
+        src: "#!",
+    },
+    {
+        id: 3,
+        logo: <Rss size={30} strokeWidth={1} />,
+        src: "#!",
+    },
+  
+];
+
+
+export const itemsNavbar = [
+    {
+        id: 1,
+        title: "Home",
+        icon: <HomeIcon size={25} color="#fff" strokeWidth={1} />,
+        link: "/",
+    },
+    {
+        id: 2,
+        title: "User",
+        icon: <UserRound size={25} color="#fff" strokeWidth={1} />,
+        link: "/about-me",
+    },
+    {
+        id: 3,
+        title: "Book",
+        icon: <BookText size={25} color="#fff" strokeWidth={1} />,
+        link: "/services",
+    },
+    {
+        id: 4,
+        title: "Target",
+        icon: <CodeSquare size={25} color="#fff" strokeWidth={1} />,
+        link: "/portfolio",
+    },
+    
+];
+
+export const dataAboutPage = [
+    {
+        id: 1,
+        title: "Frontend Developer",
+        subtitle: "TechSolutions",
+        description: "Colabora con un equipo dinámico para desarrollar interfaces de usuario atractivas y funcionales que impulsen el éxito de nuestros clientes en el mundo digital.",
+        date: "jun 2023 ",
+    },
+    {
+        id: 2,
+        title: "Creador de Experiencias Digitales",
+        subtitle: "PixelCrafters",
+        description: "Trabaja en proyectos emocionantes que desafían los límites de la creatividad y la tecnología. Únete a nosotros mientras creamos experiencias digitales cautivadoras que inspiran y cautivan a nuestros usuarios.",
+        date: "sept 2023",
+    },
+    {
+        id: 3,
+        title: "Especialista en Desarrollo Frontend",
+        subtitle: "CodeForge Solutions",
+        description: "Como desarrollador frontend, tendrás la oportunidad de colaborar en proyectos diversos y desafiantes que te permitirán expandir tus habilidades y dejar tu huella en el mundo digital.",
+        date: "nov 2023",
+    },
+    
+]
+
+export const dataCounter = [
+    {
+        id: 0,
+        endCounter: 1,
+        text: "Años de experiencia",
+        lineRight: true,
+        lineRightMobile: true,
+    },
+    {
+        id: 1,
+        endCounter: 8,
+        text: "Clientes satisfechos",
+        lineRight: true,
+        lineRightMobile: false,
+    },
+    {
+        id: 2,
+        endCounter: 10,
+        text: "Proyectos finalizados",
+        lineRight: true,
+        lineRightMobile: true,
+    },
+    
+];
+
+export const serviceData = [
+    
+    {
+        icon: <Pencil />,
+        title: "Diseño web",
+        description: "Diseño creativo y profesional de interfaces web intuitivas y atractivas, centradas en la experiencia del usuario",
+    },
+    {
+        icon: <Computer />,
+        title: "Desarrollo web",
+        description: "Diseño y desarrollo de sitios web a medida, adaptados a tus necesidades",
+    },
+    {
+        icon: <Book />,
+        title: "Copywriting",
+        description: "Creación de contenido persuasivo y atractivo que capta la atención de tu audiencia",
+    },
+    
+];
+
+export const dataPortfolio = [
+    
+    {
+        id: 2,
+        title: "Desarrollo Web Ágil",
+        image: "/image-2.jpg",
+        urlGithub: "#!",
+        urlDemo: "#!",
+    },
+    {
+        id: 3,
+        title: "Estrategias Web",
+        image: "/image-3.jpg",
+        urlGithub: "#!",
+        urlDemo: "#!",
+    },
+    {
+        id: 4,
+        title: "Ideas Creativas",
+        image: "/image-4.jpg",
+        urlGithub: "#!",
+        urlDemo: "#!",
+    },
+    {
+        id: 5,
+        title: "Webs Impactantes",
+        image: "/image-5.jpg",
+        urlGithub: "#!",
+        urlDemo: "#!",
+    },
+    {
+        id: 6,
+        title: "Web Dinámica",
+        image: "/image-6.jpg",
+        urlGithub: "#!",
+        urlDemo: "#!",
+    },
+    {
+        id: 7,
+        title: "Dark Web ",
+        image: "/image-7.jpg",
+        urlGithub: "#!",
+        urlDemo: "#!",
+    },
+];
+
+export const dataTestimonials = [
+    {
+        id: 1,
+        name: "George Snow",
+        description:
+            "¡Increíble plataforma! Los testimonios aquí son genuinos y me han ayudado a tomar decisiones informadas. ¡Altamente recomendado!",
+        imageUrl: "/profile1.png",
+    },
+    {
+        id: 2,
+        name: "Juan Pérez",
+        description:
+            "Me encanta la variedad de testimonios disponibles en esta página. Es inspirador ver cómo otras personas han superado desafíos similares a los míos. ¡Gracias por esta invaluable fuente de motivación!",
+        imageUrl: "/profile2.png",
+    },
+    {
+        id: 3,
+        name: "María García",
+        description:
+            "Excelente recurso para obtener opiniones auténticas sobre diferentes productos y servicios. Me ha ayudado mucho en mis compras en línea. ¡Bravo por este sitio!",
+        imageUrl: "/profile3.png",
+    },
+    {
+        id: 4,
+        name: "Laura Snow",
+        description:
+            "¡Qué descubrimiento tan fantástico! Los testimonios aquí son honestos y detallados. Me siento más seguro al tomar decisiones después de leer las experiencias compartidas por otros usuarios.",
+        imageUrl: "/profile4.png",
+    },
+    {
+        id: 5,
+        name: "Carlos Sánchez",
+        description:
+            "Una joya en la web. Los testimonios son fáciles de encontrar y están bien organizados. ¡Definitivamente mi destino número uno cuando necesito referencias confiables!",
+        imageUrl: "/profile5.png",
+    },
+    {
+        id: 6,
+        name: "Antonio Martínez",
+        description:
+            "¡Fantástico recurso para aquellos que buscan validación antes de tomar decisiones importantes! Los testimonios aquí son veraces y realmente útiles. ¡Gracias por simplificar mi proceso de toma de decisiones!",
+        imageUrl: "/profile6.png",
+    },
+];
